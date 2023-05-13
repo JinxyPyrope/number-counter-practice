180e542f8fa874ecef181cd3ae3b6877a5de8085
Nhoj <nhojplummer@yahoo.com>
2023-05-13 14:47:30 -0400

    Practice challenge completed

--- number-counter/src/components/Counter/counter.js
+++ number-counter/src/components/Counter/counter.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react"
+import "./counter.css"
 
 function Counter(props) {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Copy That</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Deacrease</button>
+      <p>Counter: {count}</p>
     </div>
   )
 }
